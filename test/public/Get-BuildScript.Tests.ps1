3459789ceaa05e47e86eb126cc9a543602bc56fd
Paul Broadwith <paul@pauby.com>
2018-03-19 14:29:31 +0000

    Initial commit

---
+++ test/public/Get-BuildScript.Tests.ps1
@@ -0,0 +1,21 @@
+$buildOutput = Join-Path -Path $PSScriptRoot -ChildPath '..\..\buildoutput'
+$latestBuildVersion = (Get-Childitem $buildOutput | `
+        Select-Object -Property @{ l = 'Name'; e = { [version]$_.Name } } | Sort-Object Name -Descending | `
+        Select-Object -First 1).Name.ToString()
+if ($latestBuildVersion -eq '') {
+    throw 'Cannot find the latest build of the module. Did you build it beforehand?'
+}
+else {
+    Import-Module -FullyQualifiedName (Join-Path -Path (Join-Path -Path $buildOutput -ChildPath $latestBuildVersion) `
+        -ChildPath 'psmodulebuildhelper.psd1') -Force
+}
+
+Describe 'Function Testing - Get-BuildScript' {
+    Context 'Output' {
+        It 'should pass if the build script exists and is of the correct type' {
+            $result = Get-BuildScript 
+            $result | Should -Exist
+            $result | Should -BeOfType [System.IO.FileInfo]
+        }
+    }
+} 
