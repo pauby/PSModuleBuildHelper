74b4396851c52e0aba085d816d7c709035c3499c
Paul Broadwith <paul@pauby.com>
2018-04-23 20:31:10 +0100

    Removed importing modules after installing as it wa failing tests

--- source/public/Initialize-BuildDependency.ps1
+++ source/public/Initialize-BuildDependency.ps1
@@ -71,7 +71,6 @@ function Initialize-BuildDependency {
             switch ($type) {
                 'Module' {
                     Install-DependentModule @params | Out-Null
-                    Import-Module -Name $dep.name
                     break
                 }
                 'Chocolatey' {
