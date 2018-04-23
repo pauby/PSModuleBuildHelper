777ea9c24b4b898c574fe08afe774f35aa998725
Paul Broadwith <paul@pauby.com>
2018-04-23 20:06:46 +0100

    Removed installing dependencies

--- build/Initialize-Build.ps1
+++ build/Initialize-Build.ps1
@@ -13,42 +13,8 @@ Write-Verbose 'Bootstrapping NuGet package provider.'
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 Set-PSRepository -Name PSGallery -InstallationPOlicy Trusted
 
-$requiredModules | ForEach-Object {
-        Write-Verbose "Installing module '$_'."
-        Install-Module -Name $_ -SkipPublisherCheck -AllowClobber
-        Import-Module -Name $_
-}
+Install-Module -Name PSModuleBuildHelper, InvokeBuild
 
-if (@($chocoPackages.count) -gt 0) {
-    # Check if Chocolatey is installed
-    $chocoInstalled = $true
-    try {
-        Write-Verbose 'Checking if Chocolatey is installed'
-        Invoke-Expression -Command 'choco.exe' | Out-Null
-    }
-    catch {
-        try {
-            Write-Verbose 'Chocolatey not installed. Installing.'
-            # taken from https://chocolatey.org/install
-            Set-ExecutionPolicy Bypass -Scope Process -Force
-            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        }
-        catch {
-            $chocoInstalled = $false
-            Write-Verbose 'Could not install Chocolatey'
-        }
-    }
-    
-    if ($chocoInstalled) {
-        'pandoc', '7zip' | ForEach-Object {
-            Write-Verbose "Installing '$_' package."
-            choco install $_ -y
-        }
-    
-        Write-Verbose 'Refreshing the PATH'
-        refreshenv
-    }
-} #end if
 
 # Configure git
 if ($null -eq (Invoke-Expression -Command 'git config --get user.email')) {
